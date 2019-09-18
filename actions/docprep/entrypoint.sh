da26425a0dd74dc5fd4f80373879e3f0c61d3136
caroline.sena@traycorp.com.br <caroline.sena@traycorp.com.br>
2019-09-18 10:08:30 -0300

    tentativa de CI 5

--- actions/docprep/entrypoint.sh
+++ actions/docprep/entrypoint.sh
@@ -1,28 +1,21 @@
 #!/bin/sh -l
 
+echo "Updating..."
 apt-get update
-apt-get install -y curl gnupg gnupg2 gnupg1 git
-curl -sL https://deb.nodesource.com/setup_11.x | bash -
-apt-get install -y nodejs npm
-npm install typedoc --save-dev --global
-npm install type2docfx --save-dev --global
-mkdir _package
-rm -rf docfx_project/api/*.yml
-npm install $TARGET_PACKAGE --prefix _package
-typedoc --mode file --json _output.json _package/node_modules/${TARGET_PACKAGE}/${TARGET_SOURCE_PATH} --ignoreCompilerErrors --includeDeclarations --excludeExternals
-type2docfx _output.json docfx_project/api/
+apt-get install -y unzip wget gnupg gnupg2 gnupg1
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime
+echo $TZ > /etc/timezone
 
-echo "_package/node_modules/${TARGET_PACKAGE}/${TARGET_SOURCE_PATH}"
+# Install Mono 
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | tee /etc/apt/sources.list.d/mono-official-stable.list
+apt update
+apt install mono-complete --yes
 
-# Do some cleanup.
-rm -rf _package
-rm -rf _output.json
+# Get DocFX
+wget https://github.com/dotnet/docfx/releases/download/v2.39.1/docfx.zip
+unzip docfx.zip -d _docfx
+cd docs
 
-# Check in changes.
-git config --global user.email "$GH_EMAIL"
-git config --global user.name "$GH_USER"
-
-git add . --force
-git status
-git commit -m "Update auto-generated documentation."
-git push --set-upstream origin master
+# Build docs
+mono ../_docfx/docfx.exe
