b5ee2e798119d7c244f8467c140a5075d93d3110
caroline.sena@traycorp.com.br <caroline.sena@traycorp.com.br>
2019-09-20 10:04:23 -0300

    adição de tema dark

--- actions/docprep/entrypoint.sh
+++ actions/docprep/entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "Updating..."
 apt-get update
-apt-get install -y unzip wget gnupg gnupg2 gnupg1
+apt-get install -y git unzip wget gnupg gnupg2 gnupg1
 
 # Install Mono 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
