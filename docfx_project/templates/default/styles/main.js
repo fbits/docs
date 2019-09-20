aa1c2c90fa09e0440452d1124d8939c5dfd254b2
carolinesena <55450525+carolinesena@users.noreply.github.com>
2019-09-20 16:07:34 -0300

    Update main.js

--- docfx_project/templates/default/styles/main.js
+++ docfx_project/templates/default/styles/main.js
@@ -1,4 +1,5 @@
 // Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE file in the project root for full license information.
-function changeCSS(cssFile) {
-	document.getElementById('style').setAttribute('href', cssFile);
+function changeCSS() {
+	var theme = document.getElementById('theme').value
+	document.getElementById('style').setAttribute('href', theme);
 }
