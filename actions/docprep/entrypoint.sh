466d5826ecd062f0a8a7b041699e1eec0ccf81e1
carolinesena <55450525+carolinesena@users.noreply.github.com>
2019-09-27 12:57:55 -0300

    Update entrypoint.sh

--- actions/docprep/entrypoint.sh
+++ actions/docprep/entrypoint.sh
@@ -4,27 +4,12 @@ echo "Updating..."
 apt-get update
 apt-get install -y git unzip wget gnupg gnupg2 gnupg1
 
-# Install Mono 
-:'apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | tee /etc/apt/sources.list.d/mono-official-stable.list
-apt update
-apt install mono-complete --yes
-
-# Get DocFX
-wget https://github.com/dotnet/docfx/releases/download/v2.39.1/docfx.zip
-unzip docfx.zip -d _docfx
-cd docfx_project
-
-# Build docs
-mono ../_docfx/docfx.exe
-
-cd ..
-rm -rf _docfx
-rm -f docfx.zip'
-
 # Check in changes.
 git config --global user.email "$GH_EMAIL"
 git config --global user.name "$GH_USER"
+
+echo %GH_EMAIL
+echo %GH_USER
 
 git init
 git add . --force
