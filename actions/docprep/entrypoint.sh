0894beb35a0717170476b7c7e0f49a1b153da576
carolinesena <55450525+carolinesena@users.noreply.github.com>
2019-09-27 15:01:11 -0300

    Update entrypoint.sh

--- actions/docprep/entrypoint.sh
+++ actions/docprep/entrypoint.sh
@@ -4,16 +4,30 @@ echo "Updating..."
 apt-get update
 apt-get install -y git unzip wget gnupg gnupg2 gnupg1
 
-# Check in changes.
-git config --global user.email "$GH_EMAIL"
-git config --global user.name "$GH_USER"
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | tee /etc/apt/sources.list.d/mono-official-stable.list
+apt update
+apt install mono-complete --yes
 
-echo "${GH_EMAIL}"
-echo "$GH_USER"
-echo ${GITHUB_ACTOR}
-echo ${GITHUB_REPOSITORY}
+# Get DocFX
+wget https://github.com/dotnet/docfx/releases/download/v2.39.1/docfx.zip
+unzip docfx.zip -d _docfx
+cd docfx_project
 
+# Build docs
+mono ../_docfx/docfx.exe
+cd ..
+rm -rf _docfx
+rm -f docfx.zip
+
+remote_repo="https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+remote_branch="master"
+
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+
+git init
 git add . --force
 #git status
-git commit -m "Update auto-generated documentation."
-git push origin master
+git commit -m "Deploy ${GITHUB_REPOSITORY} to ${GITHUB_REPOSITORY}:$remote_branch"
+git push --force "${remote_repo}" master:${remote_branch}
