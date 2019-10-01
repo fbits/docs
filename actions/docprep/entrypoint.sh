e2f813271037bc58efeabe527af0d066a34f4e68
carolinesena <55450525+carolinesena@users.noreply.github.com>
2019-10-01 15:09:29 -0300

    Update entrypoint.sh

--- actions/docprep/entrypoint.sh
+++ actions/docprep/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -l
 
 echo "Updating..."
-apt-get update
-apt-get install -y git unzip wget gnupg gnupg2 gnupg1
+#apt-get update
+#apt-get install -y git unzip wget gnupg gnupg2 gnupg1
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | tee /etc/apt/sources.list.d/mono-official-stable.list
-apt update
+#apt update
 apt install mono-complete --yes
 
 # Get DocFX
