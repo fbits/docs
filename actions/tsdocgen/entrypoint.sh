005de951018289c5f0ae1b8b5225deb92c3bb722
carolinesena <55450525+carolinesena@users.noreply.github.com>
2019-10-01 15:17:26 -0300

    Update entrypoint.sh

--- actions/tsdocgen/entrypoint.sh
+++ actions/tsdocgen/entrypoint.sh
@@ -4,10 +4,12 @@ apt-get update
 #apt-get install -y curl gnupg gnupg2 gnupg1 git
 curl -sL https://deb.nodesource.com/setup_11.x | bash -
 #apt-get install -y nodejs npm
+
 npm install typedoc --save-dev --global
 npm install type2docfx --save-dev --global
 mkdir _package
 #rm -rf docfx_project/api/*.yml
+
 npm install $TARGET_PACKAGE --prefix _package
 typedoc --mode file --json _output.json _package/node_modules/${TARGET_PACKAGE}/${TARGET_SOURCE_PATH} --ignoreCompilerErrors --includeDeclarations --excludeExternals
 type2docfx _output.json docfx_project/api/
