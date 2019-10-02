12509e89ba42a6f2d9eaf70e78593ce5e14b95b2
caroline.sena@traycorp.com.br <caroline.sena@traycorp.com.br>
2019-10-02 18:25:30 -0300

    Codigo js comentado

--- actions/docprep/entrypoint.sh
+++ actions/docprep/entrypoint.sh
@@ -22,14 +22,30 @@ cd ..
 rm -rf _docfx
 rm -f docfx.zip
 
-# faz push das mudancas para a master
-git config --global user.email "$GH_EMAIL"
-git config --global user.name "$GH_USER"
+set -e
 
-git add .
-git commit -m "Update auto-generated documentation."
-#git push origin master
+INPUT_BRANCH=${INPUT_BRANCH:-master}
+INPUT_FORCE=${INPUT_FORCE:-false}
+INPUT_DIRECTORY=${INPUT_DIRECTORY:-'.'}
+_FORCE_OPTION=''
 
+echo "Push to branch $INPUT_BRANCH";
+[ -z "${INPUT_GITHUB_TOKEN}" ] && {
+    echo 'Missing input "github_token: ${{ secrets.GITHUB_TOKEN }}".';
+    exit 1;
+};
+
+if ${INPUT_FORCE}; then
+    _FORCE_OPTION='--force'
+fi
+
+cd ${INPUT_DIRECTORY}
+
+# Ensure that the remote of the git repository of the current directory still is the repository where the github action is executed
+git remote add origin https://github.com/${GITHUB_REPOSITORY} || git remote set-url origin https://github.com/${GITHUB_REPOSITORY} || true
+
+header=$(echo -n "${INPUT_GITHUB_TOKEN}" | base64)
+git -c http.extraheader="AUTHORIZATION: basic $header" push origin HEAD:${INPUT_BRANCH} --follow-tags $_FORCE_OPTION;
 
 #remote_repo="https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 #remote_branch="master"
