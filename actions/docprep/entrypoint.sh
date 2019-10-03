e499367ab19db3dfa963f36e0543684dc609a37c
carolinesena <55450525+carolinesena@users.noreply.github.com>
2019-10-03 10:46:20 -0300

    Update entrypoint.sh

--- actions/docprep/entrypoint.sh
+++ actions/docprep/entrypoint.sh
@@ -22,17 +22,17 @@ cd ..
 rm -rf _docfx
 rm -f docfx.zip
 
-remote_repo="https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-remote_branch="master"
+#remote_repo="https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+#remote_branch="master"
 
-git init
-git config --global user.name "${GITHUB_ACTOR}"
-git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+#git init
+#git config --global user.name "${GITHUB_ACTOR}"
+#git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
-git add . --force
+#git add . --force
 
-git commit -m "Deploy ${GITHUB_REPOSITORY} to ${GITHUB_REPOSITORY}:$remote_branch"
-git push --force "${remote_repo}" master:${remote_branch}
+#git commit -m "Deploy ${GITHUB_REPOSITORY} to ${GITHUB_REPOSITORY}:$remote_branch"
+#git push --force "${remote_repo}" master:${remote_branch}
 
 #header=$(echo -n "ad-m:${INPUT_GITHUB_TOKEN}" | base64)
 #git -c http.extraheader="AUTHORIZATION: basic $header" push origin HEAD:master
