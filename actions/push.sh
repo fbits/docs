64da2c08205a99da1a62dbb5df91a51ec987ed8a
carolinesena <55450525+carolinesena@users.noreply.github.com>
2019-10-07 13:30:38 -0300

    Update push.sh

--- actions/push.sh
+++ actions/push.sh
@@ -7,8 +7,8 @@ INPUT_DIRECTORY=${INPUT_DIRECTORY:-'.'}
 _FORCE_OPTION=''
 
 echo "Push to branch $INPUT_BRANCH";
-[ -z "${INPUT_GITHUB_TOKEN}" ] && {
-    echo 'Missing input "github_token: ${{ secrets.GITHUB_TOKEN }}".';
+[ -z "${GITHUB_TOKEN}" ] && {
+    echo 'Missing input "GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}".';
     exit 1;
 };
 
@@ -21,5 +21,5 @@ cd ${INPUT_DIRECTORY}
 # Ensure that the remote of the git repository of the current directory still is the repository where the github action is executed
 git remote add origin https://github.com/${GITHUB_REPOSITORY} || git remote set-url origin https://github.com/${GITHUB_REPOSITORY} || true
 
-header=$(echo -n "ad-m:${INPUT_GITHUB_TOKEN}" | base64)
+header=$(echo -n "ad-m:${GITHUB_TOKEN}" | base64)
 git -c http.extraheader="AUTHORIZATION: basic $header" push origin HEAD:${INPUT_BRANCH} --follow-tags $_FORCE_OPTION;
