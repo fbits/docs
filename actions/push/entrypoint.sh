ca8873f113e55cf0e089b82818c801f8611daa00
carolinesena <55450525+carolinesena@users.noreply.github.com>
2019-10-07 10:52:23 -0300

    Update entrypoint.sh

--- actions/push/entrypoint.sh
+++ actions/push/entrypoint.sh
@@ -1,27 +1,9 @@
-#!/bin/sh
-set -e
+remote_repo="https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+remote_branch="master"
+git init
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git add . --force
 
-sudo apt-get install -y git
-
-INPUT_BRANCH=${INPUT_BRANCH:-master}
-INPUT_FORCE=${INPUT_FORCE:-false}
-INPUT_DIRECTORY=${INPUT_DIRECTORY:-'.'}
-_FORCE_OPTION=''
-
-echo "Push to branch $INPUT_BRANCH";
-[ -z "${INPUT_GITHUB_TOKEN}" ] && {
-    echo 'Missing input "github_token: ${{ secrets.GITHUB_TOKEN }}".';
-    exit 1;
-};
-
-if ${INPUT_FORCE}; then
-    _FORCE_OPTION='--force'
-fi
-
-cd ${INPUT_DIRECTORY}
-
-# Ensure that the remote of the git repository of the current directory still is the repository where the github action is executed
-git remote add origin https://github.com/${GITHUB_REPOSITORY} || git remote set-url origin https://github.com/${GITHUB_REPOSITORY} || true
-
-header=$(echo -n "${INPUT_GITHUB_TOKEN}" | base64)
-git -c http.extraheader="AUTHORIZATION: basic $header" push origin HEAD:${INPUT_BRANCH} --follow-tags $_FORCE_OPTION;
+git commit -m "Deploy ${GITHUB_REPOSITORY} to ${GITHUB_REPOSITORY}:$remote_branch"
+git pull "${remote_repo}" master
